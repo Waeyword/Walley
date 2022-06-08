5a748f81779935f0116c040f14c61cada1d671b3
Waeyword <walldoyeamafaye@gmail.com>
2022-06-08 10:58:51 +0100

    virtual env

--- walley/walley/asgi.py
+++ walley/walley/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for walley project.
+ASGI config for Walley project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walley.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Walley.settings')
 
 application = get_asgi_application()
--- walley/walley/wsgi.py
+++ walley/walley/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for walley project.
+WSGI config for Walley project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walley.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Walley.settings')
 
 application = get_wsgi_application()
